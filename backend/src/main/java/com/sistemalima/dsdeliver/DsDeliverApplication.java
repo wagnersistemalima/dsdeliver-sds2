ad8a0f06d9311564263a3e5b8189ca635384bbb8
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-01-04 23:52:52 -0300

    Project ds deliver created

---
+++ backend/src/main/java/com/sistemalima/dsdeliver/DsDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.sistemalima.dsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsDeliverApplication.class, args);
+	}
+
+}
