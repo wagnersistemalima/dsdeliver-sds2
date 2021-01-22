4765a1f7333e221411d0fe829d7ebb08317b8e72
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-01-22 12:46:05 -0300

    Page Home mobile

--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,16 +1,21 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image,} from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
 
 
 function Home() {
 
+    const navigation = useNavigation();
+
     const handleOnPress = () => {
-        
+        navigation.navigate('Orders')
     }
   return (
         <>
+            <Header />
             <View style = {styles.container}>
                 
                 <Image source={require('../assets/deliveryman.png')}/>
@@ -22,16 +27,13 @@ function Home() {
                 </Text>
                 
             </View>
+            
             <View style = {styles.footer}>
-                <RectButton style = {styles.button} onPress = {}>
+                <RectButton style = {styles.button} onPress = {handleOnPress}>
                     <Text style = {styles.buttonText}>
                         VER PEDIDOS
-
                     </Text>
-
-
                 </RectButton>
-
             </View>
 
         </>
---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import Header from "../Header"
+
+function Orders() { 
+  return (
+     <>
+            <Header />
+            <View>
+                
+              <Text>Listagem de pedidos</Text>
+
+            </View>
+
+        </>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default Orders;
